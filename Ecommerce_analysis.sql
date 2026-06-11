a3257176071857d7dca315a932775ca0af653cad
nechamamarkovitz <nechamamarkovitz@gmail.com>
2026-06-11 12:05:42 +0300

    Ecommerce_analysis.sql

--- Ecommerce_analysis.sql
+++ Ecommerce_analysis.sql
@@ -3,36 +3,34 @@ USE ecommerce_project;
 SELECT *
 FROM orders;
 
--- Profitability by payment method
-SELECT 
+-- Profitability by payment method & coupon usage
+SELECT
+    CASE
+        WHEN CouponCode IS NULL OR TRIM(CouponCode) = ''
+        THEN 'No Coupon'
+        ELSE 'Coupon Used'
+    END AS coupon_status,
     PaymentMethod,
-    SUM(TotalPrice) AS revenue,
+    ROUND(SUM(TotalPrice), 2) AS revenue,
     COUNT(*) AS orders,
     SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) AS cancelled_orders,
     ROUND(1.0 * SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) / COUNT(*), 2) AS cancellation_rate,
-    ROUND(SUM(TotalPrice) / COUNT(*), 2) AS avg_order_value
+    ROUND(100.0 * SUM(TotalPrice)/ SUM(SUM(TotalPrice)) OVER (PARTITION BY PaymentMethod), 2) AS coupon_revenue_share
 FROM Orders
-GROUP BY PaymentMethod
-ORDER BY revenue DESC;
+GROUP BY PaymentMethod, coupon_status;
 
--- Profitability by marketing channels
+-- Profitability by marketing channels & coupon usage
 SELECT 
-    ReferralSource,
-    SUM(TotalPrice) AS revenue,
-    COUNT(*) AS orders,
-    ROUND(SUM(TotalPrice) / COUNT(*), 2) AS avg_order_value
-FROM Orders
-GROUP BY ReferralSource
-ORDER BY revenue DESC;
-
--- Profitability by coupons
-SELECT 
-    CASE 
-        WHEN CouponCode IS NULL OR TRIM(CouponCode) = '' THEN 'No Coupon'
+    CASE
+        WHEN CouponCode IS NULL OR TRIM(CouponCode) = ''
+        THEN 'No Coupon'
         ELSE 'Coupon Used'
     END AS coupon_status,
-    SUM(TotalPrice) AS revenue,
+    ReferralSource,
+    ROUND(SUM(TotalPrice), 2) AS revenue,
     COUNT(*) AS orders,
-    ROUND(1.0 * SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) / COUNT(*), 2) AS cancellation_rate
+    SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) AS cancelled_orders,
+    ROUND(1.0 * SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) / COUNT(*), 2) AS cancellation_rate,
+    ROUND(100.0 * SUM(TotalPrice)/ SUM(SUM(TotalPrice)) OVER (PARTITION BY ReferralSource), 2) AS coupon_revenue_share
 FROM Orders
-GROUP BY coupon_status;
+GROUP BY ReferralSource, coupon_status;
