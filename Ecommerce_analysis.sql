1f5590935a5709fe58496fa0cc82aa3e9d32a518
nechamamarkovitz <nechamamarkovitz@gmail.com>
2026-07-05 19:46:23 +0300

    Ecommerce_analysis.sql

--- Ecommerce_analysis.sql
+++ Ecommerce_analysis.sql
@@ -3,34 +3,36 @@ USE ecommerce_project;
 SELECT *
 FROM orders;
 
--- Profitability by payment method & coupon usage
+-- coupon usage
+CREATE OR REPLACE VIEW prepped_orders AS (
+    SELECT *,
+           CASE
+               WHEN CouponCode IS NULL OR TRIM(CouponCode) = '' THEN 'No Coupon'
+               ELSE 'Coupon Used'
+           END AS coupon_status
+    FROM Orders
+ );  
+ 
+-- Profitability by payment method & coupon usage 
 SELECT
-    CASE
-        WHEN CouponCode IS NULL OR TRIM(CouponCode) = ''
-        THEN 'No Coupon'
-        ELSE 'Coupon Used'
-    END AS coupon_status,
+    coupon_status,
     PaymentMethod,
     ROUND(SUM(TotalPrice), 2) AS revenue,
     COUNT(*) AS orders,
     SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) AS cancelled_orders,
     ROUND(1.0 * SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) / COUNT(*), 2) AS cancellation_rate,
     ROUND(100.0 * SUM(TotalPrice)/ SUM(SUM(TotalPrice)) OVER (PARTITION BY PaymentMethod), 2) AS coupon_revenue_share
-FROM Orders
+FROM prepped_orders
 GROUP BY PaymentMethod, coupon_status;
 
 -- Profitability by marketing channels & coupon usage
 SELECT 
-    CASE
-        WHEN CouponCode IS NULL OR TRIM(CouponCode) = ''
-        THEN 'No Coupon'
-        ELSE 'Coupon Used'
-    END AS coupon_status,
+    coupon_status,
     ReferralSource,
     ROUND(SUM(TotalPrice), 2) AS revenue,
     COUNT(*) AS orders,
     SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) AS cancelled_orders,
     ROUND(1.0 * SUM(CASE WHEN OrderStatus = 'Cancelled' THEN 1 ELSE 0 END) / COUNT(*), 2) AS cancellation_rate,
     ROUND(100.0 * SUM(TotalPrice)/ SUM(SUM(TotalPrice)) OVER (PARTITION BY ReferralSource), 2) AS coupon_revenue_share
-FROM Orders
+FROM prepped_orders
 GROUP BY ReferralSource, coupon_status;
